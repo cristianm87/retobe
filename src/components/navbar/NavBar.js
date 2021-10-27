67eec3b82ffefe338bafccadf940dde6f163f136
adjvillegas <adj.villegas@gmail.com>
2021-10-26 22:33:44 -0300

    Agregado NavBar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+//Importar Styles
+import './NavBar.css';
+
+
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className="containerNavbar">
+            <div className="navbar">
+                <p className="firsP">Inicio</p>
+                <p className="SecondP">Nosotros</p>
+                <p className="thirtP">Catalogo</p>
+                <p className="fourP">A tu Medida</p>
+                <p className="fiveP">Contacto</p>
+            </div>
         </div>
     )
 }
