ee3ee368d95b82aea810aeacbbcb620da0e3a310
Cristian <cristianm87@hotmail.com>
2021-11-30 17:50:27 -0300

    Agregado Seccion Conoce Mas

---
+++ src/components/conocemas/Conocemas.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Conocemas.css';
+
+const Conocemas = () => {
+  return (
+    <section className="conocemas">
+      <div className="container-xxl">
+        <div className="row conocemas-bg">
+          <div className="col-lg-8 conocemas-col-img"></div>
+          <div className="col-lg-4 conocemas-col-info">
+            <div className="conocemas-info-wrapper">
+              <div className="conocemas-text-box">
+                <p>
+                  Crea <b>muebles a tu </b> <br />
+                  <b>medida</b> o solicitalos <br /> al por mayor
+                </p>
+              </div>
+              <div className="conocemas-button-wrapper">
+                <button
+                  type="button"
+                  className="btn shadow-none conocemas-button"
+                >
+                  Conoce más
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Conocemas;
--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Servicios from '../servicios/Servicios.js';
+import Conocemas from '../conocemas/Conocemas.js';
 const index = () => {
-  return <Servicios />;
+  return (
+    <>
+      <Servicios />
+      <Conocemas />
+    </>
+  );
 };
 
 export default index;
